7aeaa790121ca10597eb6ac3a08223f22fddc1d5
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-03-19 23:12:03 +0200

    chore: add Appliance model

--- manudux/models.py
+++ manudux/models.py
@@ -17,3 +17,19 @@ class Property(models.Model):
     class Meta:
         verbose_name = "Property"
         verbose_name_plural = "Properties"
+
+
+class Appliance(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    activated = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Appliance"
+        verbose_name_plural = "Appliances"
