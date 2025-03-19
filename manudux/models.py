326a0ab29bb66fdf9695ff384b700cb257947b5f
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-03-19 23:08:29 +0200

    chore: add Property model

--- manudux/models.py
+++ manudux/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Property(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    state = models.CharField(max_length=255, blank=True, null=True)
+    zip_code = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    activated = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Property"
+        verbose_name_plural = "Properties"
