f8cc0c213e86781be07dbadf485297994e4c3017
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-03-19 23:16:16 +0200

    chore: add Location model

--- manudux/models.py
+++ manudux/models.py
@@ -18,11 +18,26 @@ class Property(models.Model):
         verbose_name = "Property"
         verbose_name_plural = "Properties"
 
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    activated = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+
 
 class Appliance(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     activated = models.BooleanField(default=True)
