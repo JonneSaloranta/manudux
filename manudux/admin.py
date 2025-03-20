4afdc49c59d3a6b2c741a0050cd4daeb7bf82378
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-03-20 02:23:59 +0200

    chore: add models to admin

--- manudux/admin.py
+++ manudux/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Property, Location, Appliance, Part, Generator
+from .models import Property, Location, Appliance, Part, Generator, Stock, StockItem
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
@@ -44,5 +44,23 @@ class GeneratorAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'updated_at', 'activated')
     search_fields = ('name', 'generator')
     ordering = ('name', 'created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'created_at', 'updated_at', 'activated')
+    list_filter = ('created_at', 'updated_at', 'activated')
+    search_fields = ('name', 'location')
+    ordering = ('name', 'created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(StockItem)
+class StockItemAdmin(admin.ModelAdmin):
+    list_display = ('stock', 'part', 'quantity', 'condition', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('stock', 'part', 'quantity', 'condition')
+    ordering = ('stock', 'part', 'created_at', 'updated_at')
     date_hierarchy = 'created_at'
     readonly_fields = ('created_at', 'updated_at')
