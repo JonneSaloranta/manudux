dbf3d2ac02d50cf9f189c71de8b4172646d2aee0
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-03-23 15:30:58 +0200

    chore: add imports

--- manudux/views.py
+++ manudux/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Property, Location, Part, Appliance
+from django.contrib.auth.decorators import login_required, permission_required
 
 def index(request):
     return render(request, 'manudux/index.html')
