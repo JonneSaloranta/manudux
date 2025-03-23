89658f4280e1ee5cf50bc1f02ae94b00a8212392
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-03-23 15:32:11 +0200

    chore: update index view

--- manudux/views.py
+++ manudux/views.py
@@ -3,4 +3,6 @@ from .models import Property, Location, Part, Appliance
 from django.contrib.auth.decorators import login_required, permission_required
 
 def index(request):
-    return render(request, 'manudux/index.html')
+    context = {}
+    return render(request, 'manudux/index.html', context)
+
