8cc712eac2a519210a84084d46ac811879841b4d
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-03-23 15:33:08 +0200

    chore: add property views

--- manudux/views.py
+++ manudux/views.py
@@ -6,3 +6,13 @@ def index(request):
     context = {}
     return render(request, 'manudux/index.html', context)
 
+@login_required(login_url='/accounts/login/')
+def properties(request):
+    properties = Property.objects.all()
+    return render(request, 'manudux/properties.html', {'properties': properties})
+
+@permission_required('manudux.view_property', raise_exception=True)
+def property_detail(request, pk):
+    property = get_object_or_404(Property, pk=pk)
+    locations = property.locations.all()  # Using related_name for efficient querying
+    return render(request, 'manudux/property.html', {'property': property, 'locations': locations})
