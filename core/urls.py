0295c184168a9df9cd0861561f4487a2061d9eb1
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-03-23 15:15:50 +0200

    chore: add authentication urls

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('', include(('manudux.urls', 'manudux'), namespace='manudux')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
