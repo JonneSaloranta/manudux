45a511dbb1f61974f28a2e588c2f221ccfd02385
JonneSaloranta <saloranta.jonne@gmail.com>
2025-03-25 12:30:29 +0200

    feat: add a link for creating locations

--- manudux/urls.py
+++ manudux/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('properties/', views.properties, name='properties'),
     path('properties/create-property/', views.create_property, name='create-property'),
     path('property/<int:pk>/', views.property_detail, name='property'),
+    path('locations/create-location/', views.create_location, name='create-location'),
     path('locations/', views.locations, name='locations'),
     path('location/<int:pk>/', views.location_detail, name='location'),
     path('parts/', views.parts, name='parts'),
