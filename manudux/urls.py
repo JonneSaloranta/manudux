0d269eb78510f037b2a13b8d5700a4e5e5f1510e
Jonne Saloranta <saloranta.jonne@gmail.com>
2025-04-29 18:59:13 +0300

    feat: add site settings to app url

--- manudux/urls.py
+++ manudux/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.sign_up, name="register"),
+    path("site-settings/", views.site_settings, name="site-settings"),
     path("properties/", views.properties, name="properties"),
     path("properties/create-property/", views.create_property, name="create-property"),
     path("property/<int:pk>/edit/", views.edit_property, name="edit-property"),
